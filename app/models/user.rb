f22f832f164fbe20949be94f5ac7dbf28883a3e6
cnearing <katerina4227@gmail.com>
2022-09-05 01:18:12 +0300

    53

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,11 @@
 class User < ApplicationRecord
   has_secure_password
+
+  validates :email, presence: true, uniqueness: true
+
+  before_save :downcase_nickname
+
+  def downcase_nickname
+    nickname.downcase!
+  end
 end
