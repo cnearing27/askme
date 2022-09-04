e33dcdab137182592cb73b4a43ad9d973071d966
cnearing <katerina4227@gmail.com>
2022-09-04 18:42:39 +0300

    52

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   root to: "questions#index"
 
   resources :questions
+  resource :session, only: %i[new create destroy]
+  resources :users, only: %i[new create]
 
   resources :questions do
     member do
