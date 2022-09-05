6f81b93ac3d4f64ecb2a9cb29ce36e558c054bfd
cnearing <katerina4227@gmail.com>
2022-09-05 03:14:34 +0300

    53-1

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,11 @@
 class User < ApplicationRecord
   has_secure_password
 
-  validates :email, presence: true, uniqueness: true
-
   before_save :downcase_nickname
+
+  validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :nickname, presence: true, uniqueness: true, length: { maximum: 40 }, format: { with: /\A[0-9a-zA-Z_]+\z/ }
+
 
   def downcase_nickname
     nickname.downcase!
