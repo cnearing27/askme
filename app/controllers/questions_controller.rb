aba928b43937885ef784e3177212afbaf569f504
cnearing <katerina4227@gmail.com>
2022-09-07 22:42:17 +0300

    questions ordered by date of creating (54-1)

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -40,7 +40,7 @@ class QuestionsController < ApplicationController
   end
 
   def index
-    @questions = Question.all
+    @questions = Question.all.order(created_at: :desc)
     @question = Question.new
   end
 
