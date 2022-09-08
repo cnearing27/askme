ed8e9d70a091f23f6d83e2ae92c2bc8d0db0c4e2
cnearing <katerina4227@gmail.com>
2022-09-08 18:52:06 +0300

    user.rb codestyle

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,16 @@
 class User < ApplicationRecord
-  has_many :questions, dependent: :delete_all
+  include Gravtastic
 
   has_secure_password
-
-  include Gravtastic
-  gravtastic(secure: true, filetype: :png, size: 100, default: "identicon")
+  has_many :questions, dependent: :delete_all
 
   before_validation :downcase_nickname
 
   validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
   validates :nickname, presence: true, uniqueness: true, length: { maximum: 40 }, format: { with: /\A\w+\z/ }
   validates :header_color, format: { with: /\A#\w{6}\z/ }
+
+  gravtastic(secure: true, filetype: :png, size: 100, default: "identicon")
 
   private
 
